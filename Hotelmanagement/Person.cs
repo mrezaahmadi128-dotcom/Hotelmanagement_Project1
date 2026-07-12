ae1fce770d067069c2a0af27be3c98c3e1daf986
M_reza Ahmadi <mreza.ahmadi128@gmail.com>
2026-07-13 02:13:30 +0330

    Add project files.

---
+++ Hotelmanagement/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelmanagement
+{
+    public abstract class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string NationalCode { get; private set; }
+        public string MobileNumber { get; private set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public Person(string firstName, string lastName, string nationalCode, string mobileNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            NationalCode = nationalCode;
+            MobileNumber = mobileNumber;
+        }
+    }
+}

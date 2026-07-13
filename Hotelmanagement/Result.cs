44676b8098804c1703df9fce99183b313b259023
M_reza Ahmadi <mreza.ahmadi128@gmail.com>
2026-07-13 15:23:15 +0330

    add Class Result

---
+++ Hotelmanagement/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelmanagement
+{
+    public class Result
+    {
+        public bool Succes { get;  private set; }
+        public string Message { get; private  set; }
+        public static Result Failed(string msg)
+        {
+            Result result = new Result();
+            result.Message = msg;
+            result.Succes = false;
+            return result;
+        }
+        public static Result Ok()
+        {
+            Result result = new Result();
+            result.Succes=true;
+            return result;
+        }
+    }
+}

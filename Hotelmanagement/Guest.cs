7ea32ebd5569da1f9d0ec837fdee1cbba41598b3
M_reza Ahmadi <mreza.ahmadi128@gmail.com>
2026-07-13 17:25:25 +0330

    Add class Room and Guest

---
+++ Hotelmanagement/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelmanagement
+{
+    public class Guest:Person
+    {
+        public Guest(string firstName, string lastName,string nationalCode,string mobileNumber)
+            :base(firstName, lastName, nationalCode, mobileNumber)
+        {
+
+        }
+    }
+}
---
+++ Hotelmanagement/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelmanagement
+{
+    public class Room
+    {
+        public string RoomNumber { get; set; }
+        public int Capacity {  get; set; }
+        public decimal Price {  get; set; }
+        public Room(string roomNumber, int capacity, decimal price)
+        {
+            RoomNumber = roomNumber;
+            Capacity = capacity;
+            Price = price;
+        }
+
+    }
+}
